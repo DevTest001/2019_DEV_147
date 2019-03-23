7d7eb7b2603144e7f0632d3b5c1b43471b70da4a
Shaher Kassam <shaher.kassam@gmail.com>
2019-03-23 19:37:17 +0100

    Change interface Add gameButton Class Add actions on buttons

--- tic-tac-toe-swift/tic-tac-toe-swift/App/MainViewController.swift
+++ tic-tac-toe-swift/tic-tac-toe-swift/App/MainViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    @IBOutlet var winnerLabel: UILabel!
+    
+    private var turn: Bool = true
+    private let XImage = UIImage(named: "X")
+    private let OImage = UIImage(named: "O")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        
+        
     }
-
-
+    
+    @IBAction func buttonTouched(_ sender: UIButton) {
+        
+        buttonState(button: sender as! GameButton)
+    }
+    
+    func buttonState(button: GameButton) {
+        
+        if button.gameState == .blank {
+            
+            if turn {
+                
+                button.setBackgroundImage(XImage, for: .normal)
+                button.gameState = .X
+                turn = false
+                
+            } else {
+                
+               button.setBackgroundImage(OImage, for: .normal)
+                button.gameState = .O
+                turn = true
+                
+            }
+            
+        }
+    }
 }
+
 
